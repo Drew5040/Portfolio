21876a268e3095268f41ed08bf6cd90bc46f595c
Andrew O'Drain <andrewodrain@outlook.com>
2024-01-18 11:10:05 -0500

    add development project

---
+++ Sample7_Currency.Converter/currency_converter/app/welcome.py
@@ -0,0 +1,33 @@
+# from tkinter import Tk, ttk
+#
+#
+# # Create GUI window
+# root = Tk()
+# # Title of GUI window
+# root.title("Anwoo's Currency Converter")
+#
+# # Path to .ico file
+# icon_path = 'icon/favicon.ico'
+#
+# # Set icon
+# root.iconbitmap(icon_path)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
