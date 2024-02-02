beb65a7c9d39817e533fda3b01c8d04aaef79c0b
Andrew O'Drain <andrewodrain@outlook.com>
2024-02-02 08:17:33 -0500

    web.app.edits

--- Sample8_ML.Flask.Web.Application/static/script.js
+++ Sample8_ML.Flask.Web.Application/static/script.js
@@ -1,8 +1,29 @@
-	function validateForm() {
-		var form = document.querySelector('predictionForm');
-		if (!form.checkValidity()) {
-			alert('Please fill out all fields correctly');
-			return false;
-		}
-		return true;
-	}
+function validateForm() {
+    var form = document.getElementById('predictionForm');
+    if (!form.checkValidity()) {
+        alert('Please fill out all fields correctly');
+        return false;
+    }
+    return true;
+}
+
+function hidePrediction() {
+    var predictionElement = document.getElementById('prediction');
+    setTimeout(function() {
+        predictionElement.style.display = 'none';
+        console.log('Hiding prediction after 5 seconds...');
+    }, 5000);
+}
+
+function onSubmitActions() {
+    // Validate the form
+    var isValid = validateForm();
+
+    // If form is valid, hide the prediction after 5 seconds
+    if (isValid) {
+        setTimeout(hidePrediction, 0); // Call hidePrediction asynchronously
+    }
+
+    // Return the validation result
+    return isValid;
+}
