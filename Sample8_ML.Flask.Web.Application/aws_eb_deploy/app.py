23dc7a7b03031289b30850b89d25e976d13e040a
Andrew O'Drain <andrewodrain@outlook.com>
2024-03-06 16:46:24 -0500

    ready to deploy v0.0.1

--- Sample8_ML.Flask.Web.Application/aws_eb_deploy/app.py
+++ Sample8_ML.Flask.Web.Application/aws_eb_deploy/app.py
@@ -16,7 +16,7 @@ app = Flask(__name__)
 # Set homepage route
 @app.route('/')
 def home_page():
-    return render_template('welcome.html', message='**Please fill out all fields**')
+    return render_template('welcome.html')
 
 
 @app.route('/about')
@@ -26,8 +26,7 @@ def about_page():
 
 @app.route('/model')
 def model_page():
-    message = '**Please fill out all fields**'
-    return render_template('model.html', message=message)
+    return render_template('model.html')
 
 
 # @app.route('/contact')
@@ -61,52 +60,45 @@ def value_predictor(to_predict_list):
 
 def validate_form_entries(form_data):
     errors = {}
-    required_fields = ['age', 'w_class', 'edu', 'marital_stat', 'occupation', 'relation', 'race', 'gender',
-                       'c_gain', 'c_loss', 'hours_per_week', 'native-country']
+    required_fields = ["age", "w_class", "edu", "marital_stat", "occupation", "relation", "race", "gender",
+                       "c_gain", "c_loss", "hours_per_week", "native-country"]
 
     is_valid = True
     for field in required_fields:
-        if form_data.get(field, '') in ['', '-']:
+        if form_data.get(field, "") in ["", "-"]:
             is_valid = False
             break
 
     # Add any additional validation checks here, for example, checking if age is a number
-    if 'age' in form_data and (
-            not form_data['age'].isdigit() or not 17 <= int(form_data['age']) <= 90 or isinstance(form_data['age'],
-                                                                                                  float)):
-        errors['age'] = '**Age must be a valid number**'
+    if "age" in form_data and (not form_data["age"].isdigit() or not 17 <= int(form_data["age"]) <= 90 or isinstance(form_data["age"], float)):
+        errors["age"] = "**Age must be a valid number**"
         is_valid = False
-    if 'w_class' in form_data and form_data['w_class'] == '-':
-        errors['w_class'] = '**Please choose a category**'
-    if 'edu' in form_data and form_data['edu'] == '-':
-        errors['edu'] = '**Please choose a category**'
-    if 'marital_stat' in form_data and form_data['marital_stat'] == '-':
-        errors['marital_stat'] = '**Please choose a category**'
-    if 'occupation' in form_data and form_data['occupation'] == '-':
-        errors['occupation'] = '**Please choose a category**'
-    if 'relation' in form_data and form_data['relation'] == '-':
-        errors['relation'] = '**Please choose a category**'
-    if 'race' in form_data and form_data['race'] == '-':
-        errors['race'] = '**Please choose a category**'
-    if 'gender' in form_data and form_data['gender'] == '-':
-        errors['gender'] = '**Please choose a category**'
-    if 'c_gain' in form_data and (
-            not form_data['c_gain'].isdigit() or not 0 <= int(form_data['c_gain']) <= 99999 or isinstance(form_data,
-                                                                                                          float)):
-        errors['c_gain'] = '**Please choose a category**'
+    if "w_class" in form_data and form_data["w_class"] == "-":
+        errors["w_class"] = "**Please choose a category**"
+    if "edu" in form_data and form_data["edu"] == "-":
+        errors["edu"] = "**Please choose a category**"
+    if "marital_stat" in form_data and form_data["marital_stat"] == "-":
+        errors["marital_stat"] = "**Please choose a category**"
+    if "occupation" in form_data and form_data["occupation"] == "-":
+        errors["occupation"] = "**Please choose a category**"
+    if "relation" in form_data and form_data["relation"] == "-":
+        errors["relation"] = "**Please choose a category**"
+    if "race" in form_data and form_data["race"] == "-":
+        errors["race"] = "**Please choose a category**"
+    if "gender" in form_data and form_data["gender"] == "-":
+        errors["gender"] = "**Please choose a category**"
+    if "c_gain" in form_data and (not form_data["c_gain"].isdigit() or not 0 <= int(form_data["c_gain"]) <= 99999 or isinstance(form_data, float)):
+        errors["c_gain"] = "**Please choose a category**"
         is_valid = False
-    if 'c_loss' in form_data and (
-            not form_data['c_loss'].isdigit() or not 0 <= int(form_data['c_loss']) <= 4356 or isinstance(
-            form_data['c_loss'], float)):
-        errors['c_loss'] = '**Please choose a category**'
+    if "c_loss" in form_data and (not form_data["c_loss"].isdigit() or not 0 <= int(form_data["c_loss"]) <= 4356 or isinstance(form_data["c_loss"], float)):
+        errors["c_loss"] = "**Please choose a category**"
         is_valid = False
-    if 'hours_per_week' in form_data and (
-            not form_data['hours_per_week'].isdigit() or not 1 <= int(form_data['hours_per_week']) <= 99 or isinstance(
-            form_data['hours_per_week'], float)):
-        errors['hours_per_week'] = '**Please enter a valid number**'
+    if "hours_per_week" in form_data and (not form_data["hours_per_week"].isdigit() or not 1 <= int(form_data["hours_per_week"]) <= 99 or isinstance(form_data["hours_per_week"], float)):
+        errors["hours_per_week"] = "**Please enter a valid number**"
         is_valid = False
-    if 'native-country' in form_data and form_data['native-country'] == '-':
-        errors['native-country'] = '**Please select a category**'
+    if "native-country" in form_data and form_data["native-country"] == "-":
+        errors["native-country"] = "**Please select a category**"
+
 
     return is_valid, errors, form_data
 
@@ -123,31 +115,33 @@ def result():
 
         if not boole:
             errors_json = json.dumps(errors)
-            print('errors_json: ', errors_json)
+            print("errors_json: ", errors_json)
         else:
             errors_json = json.dumps({})
 
         if boole:
             to_predict_list = list(form_data.values())
+            print(to_predict_list)
             to_predict_list = list(map(int, to_predict_list))
-
+            print(to_predict_list)
             logging.debug(f"Received input for prediction: {to_predict_list}")
 
             results = value_predictor(to_predict_list)
 
             if int(results) == 1:
-                prediction = '**Income more than 50K**'
+                prediction = "**Income more than 50K**"
             else:
-                prediction = '**Income less than 50K**'
+                prediction = "**Income less than 50K**"
 
             logging.debug(f"Prediction result: {prediction}")
 
-            return render_template('model.html', prediction=prediction, message='**Please fill out all fields**')
+            return render_template('model.html', prediction=prediction)
 
         else:
             logging.debug('error_message accessed...')
             form_data = request.form.to_dict()
-
+            print('form_data that gets passed to error in entry template: ', form_data)
+            print('errors json fields: ', errors_json)
             return render_template('model.html',
                                    form_data=form_data,
                                    errors_json=errors_json,
--- Sample8_ML.Flask.Web.Application/static/script.js
+++ Sample8_ML.Flask.Web.Application/static/script.js
@@ -1,20 +1,35 @@
 
+// Helper function that checks for strings
 function isAlpha(value) {
     return /^[a-zA-Z]+$/.test(value);
-}
+ }
 
+// Helper function that checks for a '-'
 function isDash(value) {
     return value === '-';
-}
+ }
 
 // Function to add red border for invalid input
 function addRedBorder(inputElement) {
     inputElement.classList.add('error-input');
-}
+ }
 
+// Function to remove red border for invalid input
 function removeRedBorder(inputElement) {
-    inputElement.classList.remove('error-input')
-}
+    inputElement.classList.remove('error-input');
+ }
+
+// Function to add or take away red stars depending on input
+function toggleRedStar(show, id) {
+    var redStar = document.getElementById(id);
+    if (redStar) {
+        if (show) {
+            redStar.style.display = 'inline'; // Show the star
+     } else {
+            redStar.style.display = 'none'; // Hide the star
+     }
+   }
+ }
 
 // Validate the form by checking if all fields are filled in
 function validateForm() {
@@ -24,15 +39,16 @@ function validateForm() {
         return false;
     }
     return true;
-}
+ }
 
+// Function to hide the prediction box
 function hidePrediction() {
     var predictionElement = document.getElementById('prediction');
     console.log(predictionElement);  // Check if the element is being correctly identified
     if (predictionElement) {
         predictionElement.style.display = 'none';
     }
-}
+ }
 
  // Existing logic to hide prediction when any form field is focused
     var inputFields = document.querySelectorAll('input, select');
@@ -42,8 +58,6 @@ function hidePrediction() {
         });
     });
 
-
-
 // Real-time validation for Age
 function validateAge() {
     var ageInput = document.getElementById('age');
@@ -51,18 +65,28 @@ function validateAge() {
     var isDecimal = ageInput.value.indexOf('.') !== -1;
     if (isAlpha(age) || age < 17 || age > 90 || isDecimal) {
         addRedBorder(ageInput);
+        var show = true;
+        toggleRedStar(show, 'star0');
+
     } else {
         removeRedBorder(ageInput);
-     }
- }
+        var show = false;
+        toggleRedStar(show, 'star0');
+
+    }
+  }
 
 // Real-time validation for Occupational Sector
 function validateOs() {
     var osInput = document.getElementById('w_class');
     if (isDash(osInput.value)) {
         addRedBorder(osInput);
+        var show = true;
+        toggleRedStar(show, 'star1');
     } else {
         removeRedBorder(osInput);
+        var show = false;
+        toggleRedStar(show, 'star1');
     }
   }
 
@@ -71,8 +95,14 @@ function validateEdu() {
     var eduInput = document.getElementById('edu');
     if (isDash(eduInput.value)) {
         addRedBorder(eduInput);
+        var show = true;
+        var id = 'star2';
+        toggleRedStar(show, id);
     } else {
         removeRedBorder(eduInput);
+        var show = false;
+        var id = 'star2';
+        toggleRedStar(show, id);
     }
   }
 
@@ -81,8 +111,13 @@ function validateMarStat() {
     var msInput = document.getElementById('marital_stat');
     if (isDash(msInput.value)) {
         addRedBorder(msInput);
+        var show = true;
+        toggleRedStar(show, 'star3');
     } else {
         removeRedBorder(msInput);
+        var show = false;
+        var id = 'star3';
+        toggleRedStar(show, 'star3');
     }
   }
 
@@ -91,8 +126,14 @@ function validateOcc() {
     var occInput = document.getElementById('occupation');
     if (isDash(occInput.value)) {
         addRedBorder(occInput);
+        var show = true;
+        var id = 'star4';
+        toggleRedStar(show, id);
     } else {
         removeRedBorder(occInput);
+        var show = false;
+        var id = 'star4';
+        toggleRedStar(show, id);
     }
   }
 
@@ -101,8 +142,14 @@ function validateRel() {
     var relInput = document.getElementById('relation');
     if (isDash(relInput.value)) {
         addRedBorder(relInput);
+        var show = true;
+        var id = 'star5';
+        toggleRedStar(show, id);
     } else {
         removeRedBorder(relInput);
+        var show = false;
+        var id = 'star5';
+        toggleRedStar(show, id);
     }
   }
 
@@ -111,8 +158,14 @@ function validateRace() {
     var raceInput = document.getElementById('race');
     if (isDash(raceInput.value)) {
         addRedBorder(raceInput);
+        var show = true;
+        var id = 'star6';
+        toggleRedStar(show, id);
     } else {
         removeRedBorder(raceInput);
+        var show = false;
+        var id = 'star6';
+        toggleRedStar(show, id);
     }
   }
 
@@ -121,8 +174,14 @@ function validateGender() {
     var genderInput = document.getElementById('gender');
     if (isDash(genderInput.value)) {
         addRedBorder(genderInput);
+        var show = true;
+        var id = 'star7';
+        toggleRedStar(show, id);
     } else {
         removeRedBorder(genderInput);
+        var show = false;
+        var id = 'star7';
+        toggleRedStar(show, id);
     }
   }
 
@@ -133,8 +192,14 @@ function validateGain() {
     var isDecimal = gainInput.value.indexOf('.') !== -1;
     if (isAlpha(gain) || gain < 0 || gain > 99999 || isDecimal ) {
         addRedBorder(gainInput);
+        var show = true;
+        var id = 'star8';
+        toggleRedStar(show, id);
     } else {
         removeRedBorder(gainInput);
+        var show = false;
+        var id = 'star8';
+        toggleRedStar(show, id);
     }
   }
 
@@ -145,8 +210,14 @@ function validateLoss() {
     var isDecimal = lossInput.value.indexOf('.') !== -1;
     if (isAlpha(loss) || loss < 0 || loss > 4356 || isDecimal ) {
         addRedBorder(lossInput);
+        var show = true;
+        var id = 'star9';
+        toggleRedStar(show, id);
     } else {
         removeRedBorder(lossInput);
+        var show = false;
+        var id = 'star9';
+        toggleRedStar(show, id);
     }
   }
 
@@ -157,37 +228,83 @@ function validateHours() {
     var isDecimal = hoursInput.value.indexOf('.') !== -1;
     if (isAlpha(hours) || hours < 1 || hours > 99 || isDecimal) {
         addRedBorder(hoursInput);
+        var show = true;
+        var id = 'star10';
+        toggleRedStar(show, id);
     } else {
         removeRedBorder(hoursInput);
+        var show = false;
+        var id = 'star10';
+        toggleRedStar(show, id);
     }
-}
+  }
 
 // Real-time validation for Native Country
 function validateNat() {
     var natInput = document.getElementById('native-country');
     if (isDash(natInput.value)) {
         addRedBorder(natInput);
+        var show = true;
+        var id = 'star11';
+        toggleRedStar(show, id);
     } else {
         removeRedBorder(natInput);
+        var show = false;
+        var id = 'star11';
+        toggleRedStar(show, id);
     }
   }
 
 // Attach input event listeners for real-time validation
-document.getElementById('age').addEventListener('input', validateAge);
-document.getElementById('w_class').addEventListener('input', validateOs);
-document.getElementById('edu').addEventListener('input', validateEdu);
-document.getElementById('marital_stat').addEventListener('input', validateMarStat);
-document.getElementById('occupation').addEventListener('input', validateOcc);
-document.getElementById('relation').addEventListener('input', validateRel);
-document.getElementById('race').addEventListener('input', validateRace);
-document.getElementById('gender').addEventListener('input', validateGender);
-document.getElementById('c_gain').addEventListener('input', validateGain);
-document.getElementById('c_loss').addEventListener('input', validateLoss);
-document.getElementById('hours_per_week').addEventListener('input', validateHours);
-document.getElementById('native-country').addEventListener('input', validateNat);
+if (document.getElementById('age')) {
+    document.getElementById('age').addEventListener('input', validateAge);
+}
 
+if (document.getElementById('w_class')) {
+    document.getElementById('w_class').addEventListener('input', validateOs);
+}
+if (document.getElementById('edu')) {
+    document.getElementById('edu').addEventListener('input', validateEdu);
+}
+
+if (document.getElementById('marital_stat')) {
+    document.getElementById('marital_stat').addEventListener('input', validateMarStat);
+}
+
+if (document.getElementById('occupation')) {
+    document.getElementById('occupation').addEventListener('input', validateOcc);
+}
+
+if (document.getElementById('relation')) {
+    document.getElementById('relation').addEventListener('input', validateRel);
+}
+
+if (document.getElementById('race')) {
+    document.getElementById('race').addEventListener('input', validateRace);
+}
+
+if (document.getElementById('gender')) {
+    document.getElementById('gender').addEventListener('input', validateGender);
+}
+
+if (document.getElementById('c_gain')) {
+    document.getElementById('c_gain').addEventListener('input', validateGain);
+}
+
+if (document.getElementById('c_loss')) {
+    document.getElementById('c_loss').addEventListener('input', validateLoss);
+}
+
+if (document.getElementById('hours_per_week')) {
+    document.getElementById('hours_per_week').addEventListener('input', validateHours);
+}
+
+if (document.getElementById('native-country')) {
+    document.getElementById('native-country').addEventListener('input', validateNat);
+}
+
+// Function to run all validations at once
 function validateAllFields() {
-
     validateAge();
     validateOs();
     validateEdu();
@@ -200,18 +317,9 @@ function validateAllFields() {
     validateLoss();
     validateHours();
     validateNat();
-
 }
 
 // Function that reevaluates each field for correct entries when content is loaded or reloaded on the page
 document.addEventListener('DOMContentLoaded', function() {
     validateAllFields();
 });
-
-
-
-
-
-
-
-
